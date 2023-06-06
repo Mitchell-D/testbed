4fe838b470ae924b8361898adde2523989cc8299
Mitchell-D <mtd0012@uah.edu>
2023-06-06 08:28:29 -0500

    Methods for interfacing with a time series, other helpers

--- get_nldas2.py
+++ get_nldas2.py
@@ -10,8 +10,13 @@ from http.cookiejar import CookieJar
 import urllib
 
 gesdisc_url = "https://hydro1.gesdisc.eosdis.nasa.gov"
-nldas2_url = f"{gesdisc_url}/data/NLDAS/NLDAS_FORA0125_H.002/"
-ftemp = "NLDAS_FORA0125_H.A{YYYmmdd}.{HH}00.002.grb"
+# URL for GES DISC NLDAS2 data on a 0.125deg resolution grid
+nldas2_url = f"{gesdisc_url}/data/NLDAS/NLDAS_FORA0125_H.002"
+nldas2_template = "NLDAS_FORA0125_H.A{YYYmmdd}.{HH}00.002.grb"
+# URL for GES DISC run of Noah-LSM on the NLDAS2 domain
+noahlsm_url = f"{gesdisc_url}/data/NLDAS/NLDAS_NOAH0125_H.002"
+noahlsm_template = "NLDAS_NOAH0125_H.A{YYYmmdd}.{HH}00.002.grb"
+
 
 """
 -1:7:12:130
@@ -27,6 +32,34 @@ ftemp = "NLDAS_FORA0125_H.A{YYYmmdd}.{HH}00.002.grb"
  33:UGRD:10-m above ground Zonal wind speed [m/s]
  34:VGRD:10-m above ground Meridional wind speed [m/s]
 """
+"""
+"""
+
+def hourly_noahlsm_urls(t0:dt, tf:dt):
+    """
+    Returns a list of URLs to hourly Noah LSM files in the EOSDIS archive,
+    corresponding to the provided inclusive initial and exclusive final times.
+
+    This method only compiles URL strings based on Goddard's HTTP standard,
+    so there is no garuntee that the returned URLs actually link to an existing
+    data file.
+
+    Sub-hour time bound components are rounded down to the nearest whole hour.
+
+    :@param: inclusive initial time of data range (up to hour precision)
+    :@param: exclusive final time of data range (up to hour precision)
+    """
+    assert tf>t0
+    # Round down to the nearest whole hour.
+    t0 = dt(year=t0.year, month=t0.month, day=t0.day, hour=t0.hour)
+    tf = dt(year=tf.year, month=tf.month, day=tf.day, hour=tf.hour)
+    # Iterate over hours in the time range
+    fhours = int(((tf-t0).total_seconds()))//3600
+    ftimes = [ t0+td(hours=h) for h in range(fhours) ]
+    return [f"{noahlsm_url}/{t.year}/{t.strftime('%j')}/"+\
+            noahlsm_template.format(YYYmmdd=t.strftime("%Y%m%d"),
+                                    HH=t.strftime("%H"))
+            for t in ftimes]
 
 def hourly_nldas2_urls(t0:dt, tf:dt):
     """
@@ -50,7 +83,8 @@ def hourly_nldas2_urls(t0:dt, tf:dt):
     fhours = int(((tf-t0).total_seconds()))//3600
     ftimes = [ t0+td(hours=h) for h in range(fhours) ]
     return [f"{nldas2_url}/{t.year}/{t.strftime('%j')}/"+\
-            ftemp.format(YYYmmdd=t.strftime("%Y%m%d"),HH=t.strftime("%H"))
+            nldas2_template.format(YYYmmdd=t.strftime("%Y%m%d"),
+                                   HH=t.strftime("%H"))
             for t in ftimes]
 
 def gesdisc_auth(username,password):
@@ -86,9 +120,10 @@ def gesdisc_curl(urls:list, dl_dir:Path, skip_if_exists:bool=True, debug=False):
         dl_path = dl_dir.joinpath(Path(u).name)
         if dl_path.exists() and skip_if_exists:
             continue
-        print(dl_path)
-        subprocess.call(shlex.split(curl_command.format(
-            url=u, dl_path=dl_path)))
+        cmd = shlex.split(curl_command.format(url=u, dl_path=dl_path))
+        if debug:
+            print(f"cmd")
+        subprocess.call(cmd)
 
 def gesdisc_download(urls:list, dl_dir:Path, auth=None, letfail:bool=True,
                   skip_if_exists:bool=True, debug=False):
@@ -183,20 +218,63 @@ def get_grib1_data(grb1_path:Path):
     return (data, wgrib(f), geo)
 
 def nldas2_to_time(nldas2_path):
-    """ Parse the second and third fields of file names as a datetime """
+    """
+    Parse the second and third fields of file names as a datetime
+
+    This method is identical to noahlsm_to_time, but separate since I'm not
+    sure whether the entire EOSDISC archive or GES DISC DAAC follow standard.
+
+    :@param: nldas2_path conforming to the GES DISC standard of having the
+        2nd and 3rd '.' -separated file name fields correspond to the date,
+        for example: "NLDAS_FORA0125_H.A20190901.0000.002.grb"
+    """
+    return dt.strptime("".join(nldas2_path.name.split(".")[1:3]),"A%Y%m%d%H%M")
+
+def noahlsm_to_time(nldas2_path):
+    """
+    Parse the second and third fields of file names as a datetime.
+
+    This method is identical to nldas2_to_time, but separate since I'm not
+    sure whether the entire EOSDISC archive or GES DISC DAAC follow standard.
+
+    :@param: noahlsm_path conforming to the GES DISC standard of having the
+        2nd and 3rd '.' -separated file name fields correspond to the date,
+        for example: "NLDAS_NOAH0125_H.A20190901.0000.002.grb"
+    """
     return dt.strptime("".join(nldas2_path.name.split(".")[1:3]),"A%Y%m%d%H%M")
 
 if __name__=="__main__":
-    datadir = Path("data/nldas2_2019")
-    sample_file = datadir.joinpath(
-            Path("NLDAS_FORA0125_H.A20190101.0000.002.grb"))
-    init_time = dt(year=2019, month=1, day=1)
-    final_time = dt(year=2020, month=1, day=1)
+    debug = True
+    data_dir = Path("data/")
+    nldas2_dir = data_dir.joinpath("nldas2_2019")
+    noahlsm_dir = data_dir.joinpath("noahlsm_2019")
+    #init_time = dt(year=2019, month=1, day=1)
+    #final_time = dt(year=2020, month=5, day=1)
+    #init_time = dt(year=2019, month=5, day=1)
+    #final_time = dt(year=2020, month=9, day=1)
+    init_time = dt(year=2019, month=9, day=1)
+    final_time = dt(year=2020, month=12, day=1)
 
     # Generate strings for each hourly nldas2 file in the time range
-    urls = hourly_nldas2_urls(t0=init_time, tf=final_time)
-    #gesdisc_curl(urls, datadir, debug=True)
+    #nldas_urls = hourly_nldas2_urls(t0=init_time, tf=final_time)
+    #gesdisc_curl(nldas_urls, nldas2_dir, debug=debug)
+
+    # Generate strings for each hourly Noah-LSM file in the time range.
+    #lsm_urls = hourly_noahlsm_urls(t0=init_time, tf=final_time)
+    # Download the Noah LSM files
+    #gesdisc_curl(lsm_urls, noahlsm_dir, debug=debug)
+
+    #'''
+    """ Print information about a sample file """
+    # NLDAS2 LSM forcings
+    #sample_file = nldas2_dir.joinpath(
+    #        Path("NLDAS_FORA0125_H.A20190101.0000.002.grb"))
+    # Noah-LSM run on NLDAS2 domain
+    sample_file = noahlsm_dir.joinpath(
+            Path("NLDAS_NOAH0125_H.A20190901.0000.002.grb"))
     for w in wgrib(sample_file):
         print(w)
-    #get_grib1_data(sample_file)
-    nldas2_to_time(sample_file)
+    data, info, geo = get_grib1_data(sample_file)
+    #nldas2_to_time(sample_file)
+    noahlsm_to_time(sample_file)
+    #'''
--- nldas2_analysis.py
+++ nldas2_analysis.py
@@ -12,21 +12,37 @@ from aes670hw2 import geo_plot as gp
 #'''
 
 from get_nldas2 import get_grib1_data, wgrib, nldas2_to_time
+from pickle_pixel_picker import pick_pixels
+
+def grib_parse_pixels(pixels:list, datafiles:list):
+    """
+    Opens a grib1 file and extracts the values of pixels at a list of
+    indeces like [ (j1,i1), (j2,i2), (j3,i3), ], returning a list of
+    tuples like [ (fname, [val1,val2,val3]), (fname, [val1,val2,val3]), ]
+    """
+    pass
 
 if __name__=="__main__":
     # Full hourly directory of FORA0125 files
     data_dir = Path("data")
-    nldas2_data_dir = data_dir.joinpath("nldas2_2019")
-    static_data_dir = data_dir.joinpath("lis_static")
+    nldas2_dir = data_dir.joinpath("nldas2_2019")
+    noahlsm_dir = data_dir.joinpath("noahlsm_2019")
+    static_dir = data_dir.joinpath("lis_static")
     fig_dir = Path("figures/")
     init_time = dt(year=2019, month=1, day=1)
     final_time = dt(year=2020, month=1, day=1)
 
     #'''
-    """ Generate sample graphics for each product """
-    lis_files = list(nldas2_data_dir.iterdir())
-    sample_file = lis_files[-1]
+    """ Open a sample file """
+    nldas_files = list(nldas2_dir.iterdir())
+    lsm_files = list(noahlsm_dir.iterdir())
+    sample_file = nldas_files[-1]
+    #sample_file = lsm_files[-1]
     data, info, geo = get_grib1_data(sample_file)
+    #'''
+
+    '''
+    """ Generate sample graphics for each product """
     for i in range(len(data)):
         #amin = np.amin(data[i])
         #data[i][np.where(type(data[i])==np.ma.core.MaskedConstant)] = amin
@@ -38,18 +54,27 @@ if __name__=="__main__":
         gp.generate_raw_image(
                 enh.norm_to_uint(tmp_rgb,256,np.uint8),
                 fig_dir.joinpath(
-                    f"sample_{info[i]['record']}_{info[i]['name']}.png"))
-        #gt.quick_render(tmp_rgb)
-    #'''
+                    f"sample_nldas2_{info[i]['record']}_{info[i]['name']}.png"))
+        gt.quick_render(tmp_rgb)
+    '''
 
+    '''
     """
-    Get a time series of LIS data for a pixel chosen with pickle_pixel_picker
+    Pick a time series pixel with pickle_pixel_picker using the LIS soil type
     """
-    # Pixel in SEUS with abundance of soil types nearby
+    # Find a pixel in SEUS or plains with consistent soil types
     # (coordinates of SPoRT-LIS pixel)
-    px_center = [(622, 1209)]
+    #lis_soiltype = pkl.load(static_dir.joinpath(
+    #    "soiltype_lis_3KM_conus.pkl").open("rb"))[::-1]
+    #pick_pixels(lis_soiltype, replace_val=-3)
+    #print(enh.array_stat(lis_soiltype))
+    #print(lis_soiltype.dtype)
+    '''
 
-    lis_soiltype = pkl.load(static_data_dir.joinpath(
-        "soiltype_lis_3KM_conus.pkl").open("rb"))
-    print(enh.array_stat(lis_soiltype))
-    print(lis_soiltype.dtype)
+    #'''
+    """
+    Pick a pixel using the  NLDAS soil type
+    """
+    px = pick_pixels(pkl.load(static_dir.joinpath(
+        "soiltype_nldas_14km_conus.pkl").open("rb"))[::-1])[0]
+    #'''
