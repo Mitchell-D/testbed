9ed01b7d2596e6d205698b6fa200a2e1aa13838b
Mitchell-D <mtd0012@uah.edu>
2023-07-07 17:07:08 -0500

    Finished+tested reliable GeoTimeSeries databasing system

--- GeoTimeSeries.py
+++ GeoTimeSeries.py
@@ -1,91 +1,167 @@
-"""
-Abstract class for
-"""
 import pickle as pkl
 import numpy as np
+import string
+import re
 from pathlib import Path
 from datetime import datetime
 from datetime import timedelta
-from typing import Callable
-
-recipes = {}
 
 class GeoTimeSeries:
     """
-    Abstract class for organizing and interfacing with multiple independent
-    and dependent data variables in a 1-dimensional time series.
-
-    Each 1d time series, static data value, or recipe is referenced with a
-    unique string label that allows a copy to be returned and enables the data
-    to be invoked in recipes.
+    GeoTimeSeries is a basic class representing a 1d time series of a scalar
+    variable corresponding to a grid point on a larger domain. GeoTimeSeries
+    objects enforce uniqueness using 4 attributes: time range, index, flabel
+    (feature label), and mlabel (modification label)
     """
-    def __init__(self, times:np.ndarray):
-        """
-        Initialize a GeoTimeSeries with an array of times used to index all
-        data added with the GeoTimeSeries.add() object method.
-        """
-        self._data = {}
-        self._static = {}
-        self._info = {}
-        self._labels = []
-        self._recipes = recipes
+    @staticmethod
+    def load(gts_file:Path):
+        gts_file = Path(gts_file)
+        return GeoTimeSeries(
+                np.load(gts_file.as_posix()),
+                *GeoTimeSeries.file_to_fields(gts_file))
 
-        # Make sure the timesteps are monotonic
-        assert all([t0<t1 for t0,t1 in zip(times[:-1],times[1:])])
-        self._time = times
-
-    @property
-    def labels(self):
-        return list(self._data.keys()) + list(self._recipes.keys()) + \
-                list(self._static.keys())
-
-    def _validate_label(self, label:str):
+    @staticmethod
+    def file_to_fields(gts_file:Path):
         """
-        Verify that the provided string label is not already either a static
-        dataset key in the data dictionary or a recipe label.
+        Strips values from GeoTimeSeries numpy serial file names per the
+        standard naming scheme, formatted as follows:
+
+        {flabel}_{mlabel}_i{initial time}_d{time interval}_s{data size}_ \
+                y{vert index}_x{horiz index}.npy
+
+        For initial time and time step fields, decimals are replaced with
+        dashes in the file name.
+
+        Information in these fields is sufficient for describing a unique
+        continuous equal-interval time series on a larger grid for a single
+        dataset with optional derived values specified by the mlabel.
+
+        :@param gts_file: Path to a GeoTimeSeries .npy serial file which
+                conforms to the standard naming scheme above. The file
+                should have been created by the .save() object attribute.
+        :@return: 4-tuple with fields like (times, idx, flabel, mlabel)
+                where times is a list of datetime objects, idx is a 2-tuple
+                of ints, and flabel/mlabel are strings. Note that the returned
+                tuple doesn't have the same order as the fields in the file
+                name; instead it's ordered like the GeoTimeSeries init params.
         """
-        if label in self._data.keys():
-            raise ValueError(f"{label} is already a dataset key")
-        if label in self._recipes.keys():
-            raise ValueError(f"{label} is already a recipe key")
-        if label in self._static.keys():
-            raise ValueError(f"{label} is already a static dataset key")
+        gts_file = Path(gts_file)
+        exp = r"([\w-]+)_([\w-]+)_i([\d-]+)_d([\d-]+)_s(\d+)_y(\d+)_x(\d+).npy"
+        match = re.search(exp, gts_file.name)
+        flabel, mlabel, t0, dt, size, yidx, xidx = match.groups()
+        t0 = datetime.fromtimestamp(float(t0.replace("-",".")))
+        dt = timedelta(seconds=float(dt.replace("-",".")))
+        times = [ t0+i*dt for i in range(int(size)) ]
+        idx = yidx, xidx
+        return (times, idx, flabel, mlabel)
+
+    @staticmethod
+    def _validate_label(label:str):
+        valid = list(string.ascii_lowercase + string.ascii_uppercase)
+        valid += list(map(str, range(10))) + ["-"]
+        if not all([c in valid for c in label]):
+            raise ValueError("All characters in a label string must be " + \
+                    f"alphanumeric or a dash. Invalid: {label}")
         return label
 
-    def add_data(self, label:str, data:np.ndarray, info:dict=None):
+    def __init__(self, data1d:np.array, times:list, idx:tuple, flabel:str,
+                 mlabel:str="default"):
         """
-        Add a 1d dataset to the collection with a string label and a 1d array
-        with the same size as this GeoTimeSeries' number of timesteps.
+        Initialize a GeoTimeSeries with a 1d data array.
 
-        :@param label: Unique string label to assign to the new data
-        :@param data: 1-dimensional numpy scalar array with the same number of
-            elements as timesteps in this GeoTimeSeries.
+        :@param data1d: 1-dimensional numpy array for a scalar time series
+        :@param times: List of datetime objects with the same length as data1d
+        :@param idx: 2-tuple integer index of the point on a larger grid from
+                which this time series was extracted
+        :@param flabel: "feature" label, used to identify a unique type of
+                data. For example: pressure, soilm-10cm, solar-flux, etc.
+                Only alphanumeric characters and dashes are allowed.
+        :@param mlabel: "modification" label, used to distinguish time series
+                of the same feature type that have been modified in some way.
+                For example, if I make a 1-week percentile of a normalized
+                time series with flabel=tsoil, the user may set the mlabel to
+                something descriptive like "pct1wk-norm".
         """
-        assert len(data.shape)==1 and data.size==len(self._time)
-        self._data[self._validate_label(label)] = np.copy(data)
+        # Make sure the dataset is indeed 1-dimensional
+        assert len(data1d.shape)==1
+        # There must be a timestep for each 1d data entry.
+        assert len(times) == data1d.size
+        # Timesteps must be uniform-interval and monotonic
+        dt = times[1]-times[0]
+        assert all([ t1-t0==dt for t0,t1 in zip(times[:-1],times[1:]) ])
+        self._data = data1d
+        self._times = times
+        self._dt = dt
+        self._idx = idx
+        self._flabel = GeoTimeSeries._validate_label(flabel)
+        self._mlabel = GeoTimeSeries._validate_label(mlabel)
 
-    def add_static(self, label:str, static, info:dict=None):
-        """
-        Add a static dataset with a unique label, which may be any
-        serializable value such as a number or a vector.
-        """
-        self._static[self._validate_label(label)] = static
+    @property
+    def mlabel(self):
+        return self._mlabel
 
-    def add_recipe(self, label:str, recipe:Callable, params:list,
-                   info:dict=None):
+    @mlabel.setter
+    def mlabel(self, mlabel):
         """
-        Add a callable recipe
-
-        (!) Need to determine whether to use string labels of callable
-            parameters as an argument... If so, all inputs will need to be
-            pre-loaded into the GTS, which causes redundancies if an external
-            array needs to be applied to many GTS instances.
-            Otherwise, there seems to be little difference from just applying
-            the method externally and referencing the data from the GTS inst.
+        Only the mlabel attribute can be changed by the user, which is useful
+        for storing modified versions of a single time series.
         """
-        self._recipes[self._validate_label(label)] = recipe
-        return None
+        self._mlabel = GeoTimeSeries._validate_label(mlabel)
 
+    @property
+    def flabel(self):
+        return self._flabel
+
+    @property
+    def idx(self):
+        return self._idx
+
+    @property
+    def data(self):
+        return self._data
+
+    def save(self, data_dir:Path, replace:bool=False):
+        """
+        Save the data array to a .npy file in the provided directory using
+        the standard file naming scheme, which enables this GeoTimeSeries to
+        be fully re-loaded with GeoTimeSeries.load()
+
+        :@param data_dir: Directory to generate the new serial array
+                representing this GeoTimeSeries
+        :@param replace: If True and a file matching all attributes exists in
+                the provided data directory, overwrites it. Raises ValueError
+                otherwise. There shouldn't be name collisions unless you're
+                actually re-generating identical time series.
+        :@return: Path object to the newly created .npy file.
+        """
+        data_dir = Path(data_dir)
+        assert data_dir.exists() and data_dir.is_dir()
+        fname  = data_dir.joinpath(self.get_file_name())
+        if fname.exists() and not replace:
+            raise ValueError(f"{fname.as_posix()} already exists!")
+        np.save(fname.as_posix(), self._data)
+        return fname
+
+    def get_file_name(self):
+        """
+        Format a numpy serial file path that enables retrieval of
+        all GeoTimeSeries attributes without storing meta-information
+
+        :@return str: standard-format GeoTimeSeries file name.
+        """
+        # first 2 fields are the flabel and mlabel
+        fname = f"{self.flabel}_{self.mlabel}_"
+        # 3rd field is the initial time in epoch seconds
+        fname += f"i{str(self._times[0].timestamp()).replace('.','-')}_"
+        # 4th field is the temporal resolution in seconds
+        fname += f"d{str(self._dt.total_seconds()).replace('.','-')}_"
+        # 5th field is the number of timesteps in total
+        fname += f"s{self._data.size}_"
+        # 6th and 7th fields are the 1st and 2nd (vertical and horizontal) idx
+        fname += f"y{self._idx[0]}_x{self._idx[1]}.npy"
+        return fname
+
+    '''
     def subset(self,label:str, t0:datetime, tf:datetime):
         """
         Returns a 1d array of the labeled time series between the provided
@@ -102,40 +178,35 @@ class GeoTimeSeries:
             if tf<=self._time[i]:
                 final_idx = i
         return self._data[label][init_idx:final_idx]
+    '''
 
 if __name__=="__main__":
     debug = True
-    data_dir = Path("data/1D")
+    data_dir = Path("data/GTS")
+    buf_dir = Path("data/buffer")
+    noahlsm_points,nldas_points,pixels,times,nldas_info,noahlsm_info=pkl.load(
+            buf_dir.joinpath("tmp2.pkl").open("rb"))
 
-    noahlsm_pkl = data_dir.joinpath("silty-loam_noahlsm_all-fields_2019.pkl")
-    nldas2_pkl = data_dir.joinpath("silty-loam_nldas2_all-forcings_2019.pkl")
+    noahlsm_points = np.stack(noahlsm_points)
+    nldas_points = np.stack(nldas_points)
+    gtss = []
 
-    #noahlsm_pkl = data_dir.joinpath("sl2_noahlsm_all-fields_2019.pkl")
-    #nldas2_pkl = data_dir.joinpath("sl2_nldas2_all-forcings_2019.pkl")
+    '''
+    for i in range(len(noahlsm_info)):
+        if noahlsm_info[i]["name"] in ("LSOIL", "SOILM"):
+            for p in range(noahlsm_points.shape[1]):
+                print(noahlsm_info[i]["name"], noahlsm_info[i]["lvl_str"])
+                flabel = noahlsm_info[i]["name"] + "-" + \
+                        noahlsm_info[i]["lvl_str"].split(" ")[0]
+                tmp_gts = GeoTimeSeries(
+                        noahlsm_points[:,p,i].data,times,pixels[p],flabel)
+                tmp_gts.save(data_dir, replace=True)
+    '''
 
-    noahlsm,_,noahlsm_info = pkl.load(noahlsm_pkl.open("rb"))
-    nldas2,_,nldas2_info = pkl.load(nldas2_pkl.open("rb"))
-
-    # For silty-loam only
-    t0 = datetime(year=2019, month=1, day=1, hour=0)
-    tf = datetime(year=2020, month=1, day=1, hour=0)
-
-    timesteps = [t0+timedelta(hours=hours) for hours in
-                 range(int((tf-t0).total_seconds() // 3600 ))]
-
-    gts = GeoTimeSeries(timesteps)
-    for i in range(noahlsm.shape[2]):
-        if noahlsm_info[i]["name"] in ("SOILM","LSOIL"):
-            tmp_name = noahlsm_info[i]["name"] + "_" + \
-                    noahlsm_info[i]["lvl_str"].split(" ")[0]
-            gts.add_data(tmp_name, noahlsm[:,0,i], noahlsm_info)
-
-    for i in range(nldas2.shape[2]):
-        gts.add_data(nldas2_info[i]["name"], nldas2[:,0,i], nldas2_info)
-
-    print(gts.labels)
-    print(gts.subset(
-        "SOILM_0-100",
-        datetime(year=2019,month=4,day=1),
-        datetime(year=2019,month=10,day=1)
-        ))
+    '''
+    for i in range(len(nldas_info)):
+        for p in range(nldas_points.shape[1]):
+            tmp_gts = GeoTimeSeries(nldas_points[:,p,i].data, times,
+                                    pixels[p], nldas_info[i]["name"])
+            tmp_gts.save(data_dir, replace=True)
+    '''
