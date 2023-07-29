4fe9f38f68939d4c553da906a85bfd0cd0278e9f
Mitchell-D <mtd0012@uah.edu>
2023-07-29 14:55:24 -0500

    Finished pixel extraction and subsetting for TimeGrid. Dataset curation system is now much more efficient and readable :)

--- test_timegrid.py
+++ test_timegrid.py
@@ -15,21 +15,38 @@ if __name__=="__main__":
     data_dir = Path("data")
     static_pkl = data_dir.joinpath("static/nldas2_static_all.pkl")
     subgrid_dir = data_dir.joinpath("subgrids")
-    static = pkl.load(static_pkl.open("rb"))
-    gt.quick_render(static["soil_comp"][64:192,200:328])
+    # Dimensions of the .npy file subgrids wrt original nldas grid
+    yrange, xrange = slice(64, 192), slice(200,328)
+    # Initial and final time of the extracted time series
+    init_time = datetime(2021, 4, 1)
+    final_time = datetime(2021, 8, 1)
+    # Feature labels of the datasets to extract. The order of these lists
+    # corresponds to the order of the features dimension in returned arrays.
+    nldas_feats = ["TMP", "PRES", "NCRAIN", "SPFH", "DLWRF", "DSWRF", "PEVAP"]
+    noahlsm_feats = ['SOILM-0-10', 'SOILM-10-40', 'SOILM-40-100',
+                     'SOILM-100-200']
 
+    # Use a GUI to select a set of pixels to extract.
+    soilpct = pkl.load(static_pkl.open("rb"))["soil_comp"][yrange,xrange]
+    pixels = gt.get_category(soilpct)
+
+    # Extract a timeseries for each selected pixels as a (T,F) shaped array
+    # for T timesteps in range (init_time, final_time) and F features.
     nldas_paths = [p for p in subgrid_dir.iterdir()
                    if p.stem.split("_")[0]=="FORA0125"]
-    noahlsm_paths = [p for p in subgrid_dir.iterdir()
-                   if p.stem.split("_")[0]=="NOAH0125"]
-
     tg_nldas = TimeGrid(
             time_file_tuples = [
                 (datetime.strptime(p.stem.split("_")[1], "%Y%m%d-%H"),p)
                 for p in nldas_paths],
             labels = ['TMP', 'SPFH', 'PRES', 'UGRD', 'VGRD', 'DLWRF',
                       'NCRAIN', 'CAPE', 'PEVAP', 'APCP', 'DSWRF']
-            )
+            ).subset(init_time,final_time)
+    nldas_arrays = tg_nldas.extract_timeseries(
+            pixels, nldas_feats, nworkers=4)
+
+    # Same thing for Noah-LSM data
+    noahlsm_paths = [p for p in subgrid_dir.iterdir()
+                   if p.stem.split("_")[0]=="NOAH0125"]
     tg_noahlsm = TimeGrid(
             time_file_tuples = [
                 (datetime.strptime(p.stem.split("_")[1], "%Y%m%d-%H"),p)
@@ -37,7 +54,10 @@ if __name__=="__main__":
             labels=['SOILM-0-100', 'SOILM-0-10', 'SOILM-10-40', 'SOILM-40-100',
                     'SOILM-100-200', 'LSOIL-0-10', 'LSOIL-10-40',
                     'LSOIL-40-100', 'LSOIL-100-200']
-            )
+            ).subset(init_time,final_time)
+    noahlsm_arrays = tg_noahlsm.extract_timeseries(
+            pixels, noahlsm_feats, nworkers=4)
 
-    tg_nldas.validate()
-    tg_noahlsm.validate()
+    feats = nldas_feats + noahlsm_feats
+    px_arrays = [np.hstack([A,B]) for A,B in zip(nldas_arrays, noahlsm_arrays)]
+    print(px_arrays[0].shape)
