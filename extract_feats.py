8dcdf909d340594f4e65a4c68c25c2058f430c5a
Mitchell-D <mtd0012@uah.edu>
2023-12-29 11:57:57 -0600

    Finished acquiring and validating data, and started setting up batch generator with selection options. Good progress.

--- extract_feats.py
+++ extract_feats.py
@@ -7,87 +7,24 @@ from pprint import pprint as ppt
 from krttdkit.acquire import grib_tools, gesdisc
 import h5py
 
-noahlsm_record_mapping = (
-        (1,"nswrs"),        ## net shortwave at surface
-        (2,"nlwrs"),        ## net longwave at surface
-        (3,"lhtfl"),        ## latent heat flux
-        (4,"shtfl"),        ## sensible heat flux
-        (5,"gflux"),        ## ground heat flux
-        (10,"arain"),       ## liquid precipitation
-        (11,"evp"),         ## evapotranspiration
-        (12,"ssrun"),       ## surface runoff
-        (13,"bgrun"),       ## sub-surface runoff
-        (19,"tsoil-10"),    ## depth-wise soil temperature
-        (20,"tsoil-40"),
-        (21,"tsoil-100"),
-        (22,"tsoil-200"),
-        (26,"soilm-10"),    ## depth-wise soil moisture content
-        (27,"soilm-40"),
-        (28,"soilm-100"),
-        (29,"soilm-200"),
-        (30,"lsoil-10"),    ## depth-wise liquid soil moisture
-        (31,"lsoil-40"),
-        (32,"lsoil-100"),
-        (33,"lsoil-200"),
-        (34,"mstav-200"),   ## moisture availability 0-200cm
-        (35,"mstav-100"),   ## moisture availability 0-100cm
-        (36,"evcw"),        ## canopy water evaporation
-        (37,"trans"),       ## transpiration
-        (38,"evbs"),        ## bare soil evaporation
-        (49,"rsmin"),       ## minimal stomatal resistance
-        (50,"lai"),         ## leaf area index
-        (51,"veg"),         ## vegetation fraction
-        )
-nldas_record_mapping = (
-        (1,"tmp"),          ## 2m temperature
-        (2,"spfh"),         ## 2m specific humidity
-        (3,"pres"),         ## surface pressure
-        (4,"ugrd"),         ## 10m zonal wind speed
-        (5,"vgrd"),         ## 10m meridional wind speed
-        (6,"dlwrf"),        ## downward longwave radiative flux
-        (7,"ncrain"),       ##
-        (8,"cape"),         ## convective available potential energy
-        (9,"pevap"),        ## hourly potential evaporation
-        (10,"apcp"),        ## hourly precip total
-        (11,"dswrf"),       ## downward shortwave radiative flux
-        )
-
-'''
-def mp_extract_nldas_subgrid(grib_files:list, file_times:list, v_bounds:slice,
-                             h_bounds:slice, out_dir:Path, records:list,
-                             data_label:str, nworkers:int=1):
-    """
-    Multiprocessed method to extract a pixel subgrid of NLDAS2-grid grib1
-    forcing files, which includes data from the NLDAS run of the Noah-LSM.
-    Extracted array subgrids are stored as ".npy" serial files
-
-    :@param grib_files: List of valid grib1 files to extract arrays from.
-    :@param file_times: List of datetimes associated with each grib1 file.
-    :@param v_bounds: Slice of vertical coordinates to subset the array.
-    :@param h_bounds: Slice of horizontal coordinates to subset the arrays.
-    :@param out_dir: Directory to deposit new ".npy" files in
-    :@param records: List of record numbers from wgrib for data to keep in
-        the serial files.
-    :@param data_label: Identifying string for the dataset being extracted.
-        This is the first underscore-separated field in the generated ".npy"
-        arrays, followed by the date in YYYYmmdd-HH format.
-    :@param nworkers: Number of subprocesses to spawn concurrently in order
-        to extract files.
-    """
-    assert len(file_times)==len(grib_files)
-    with mp.Pool(nworkers) as pool:
-        args = [(Path(grib_files[i]), file_times[i], v_bounds, h_bounds,
-                 out_dir, records, data_label)
-                for i in range(len(grib_files))]
-        results = pool.map(_extract_nldas_subgrid, args)
-'''
+from list_feats import nldas_record_mapping, noahlsm_record_mapping
 
 def extract_feats(nldas_grib_dir:Path, noahlsm_grib_dir:Path, out_h5file:Path,
-                  workers=1):
+                  wgrib_bin="wgrib", workers=1):
+    """
+    Multiprocessed method for converting directories of NLDAS2 and Noah-LSM
+    grib1 files (acquired from the GES DISC DAAC) into a single big hdf5 file
+    containing only the records specified above in noahlsm_record_mapping and
+    nldas_record_mapping, adhering to the order that they appear in the lists.
+
+    The files in each directory must be uniformly separated in time (ie same
+    dt between each consecutive file), and each NLDAS2 file must directly
+    correspond to a simultaneous NoahLSM file.
+    """
+    ## Refuse to overwrite an existing hdf5 output file.
+    assert not out_file.exists()
     ## Find matching-time pairs of NLDAS2 and NoahLSM grib files, and store
     ## as a list of 3-tuples like (datetime, nldas_path, noahlsm_path).
-    ## Also verify that the time steps between frames are consistent according
-    ## to the acquisition time in the file name
     nldas_files = list(sorted(
         [(gesdisc.nldas2_to_time(f),f) for f in nldas_grib_dir.iterdir()],
         key=lambda t:t[0]
@@ -102,14 +39,18 @@ def extract_feats(nldas_grib_dir:Path, noahlsm_grib_dir:Path, out_h5file:Path,
             for i in range(len(nldas_files))
             if nldas_files[i][0] == noahlsm_files[i][0]
             ]
+    ## Verify that the time steps between frames are consistent according
+    ## to the acquisition time in the file name
     times = [t[0] for t in file_pairs]
     dt = times[1]-times[0]
     assert all(b-a==dt for b,a in zip(times[1:],times[:-1]))
 
     ## Extract a sample grid
     tmp_time,tmp_nldas,tmp_noah = file_pairs[0]
-    nldas_data,nldas_info,nldas_geo = grib_tools.get_grib1_data(tmp_nldas)
-    noah_data,noah_info,noah_geo = grib_tools.get_grib1_data(tmp_noah)
+    nldas_data,nldas_info,nldas_geo = grib_tools.get_grib1_data(
+            tmp_nldas, wgrib_bin=wgrib_bin)
+    noah_data,noah_info,noah_geo = grib_tools.get_grib1_data(
+            tmp_noah, wgrib_bin=wgrib_bin)
 
     h5_shape = (
             len(times),
@@ -119,66 +60,85 @@ def extract_feats(nldas_grib_dir:Path, noahlsm_grib_dir:Path, out_h5file:Path,
     nl_recs,nl_labels = map(list,zip(*nldas_record_mapping))
     no_recs,no_labels = map(list,zip(*noahlsm_record_mapping))
 
-    f = h5py.File(out_file.as_posix(), "w-")
-    g = f.create_group("/data")
-    d = g.create_dataset(
-            name="feats",
-            shape=h5_shape,
-            chunks=(24,1,1,h5_shape[-1]),
-            compression="gzip",
-            )
-    d.dims[0].label = "time"
-    d.dims[1].label = "lat"
-    d.dims[2].label = "lon"
-    d.attrs["labels"] = nl_labels + no_labels
+    print(f"hdf5 feature shape: {h5_shape}")
 
-    fill_count = 0
+    ## Use a ~64 MB cache
+    csize = 64*1024**2
+    with h5py.File(out_file.as_posix(), "w-", rdcc_nbytes=csize) as f:
+        g = f.create_group("/data")
+        d = g.create_dataset(
+                name="feats",
+                shape=h5_shape,
+                chunks=(24,16,16,h5_shape[-1]),
+                compression="gzip",
+                )
+        d.dims[0].label = "time"
+        d.dims[1].label = "lat"
+        d.dims[2].label = "lon"
+        d.attrs["labels"] = nl_labels + no_labels
 
-    with mp.Pool(workers) as pool:
-        args = [(nl,no,nl_recs,no_recs)
-                for t,nl,no in file_pairs]
-        for r in pool.imap(_parse_file, args):
-            d[fill_count] = r
-            print(f"extracted {times[fill_count]}")
-            print(r.shape)
-            fill_count += 1
+        fill_count = 0
+
+        with mp.Pool(workers) as pool:
+            args = [(nl,no,nldas_info,noah_info,nl_recs,no_recs)
+                    for t,nl,no in file_pairs]
+            for r in pool.imap(_parse_file, args):
+                d[fill_count:fill_count+1,:,:,:] = r
+                print(f"Completed timestep {times[fill_count]}")
+                fill_count += 1
 
 def _parse_file(args:tuple):
     """
-    Extract the specified nldas grib1 file and return the requested records
-    as a uniform-size 3d array for each timestep like (heigh, width, feature).
+    Extract the specified nldas and noahlsm grib1 files and return the
+    requested records as a uniform-size 3d array for each timestep like
+    (lat, lon, feature) in the same order as the provided records, with
+    all nldas records coming first, then noahlsm records.
 
-    The
+    Note that this method only returns the consequent numpy array, so the
+    record labels, geolocation, info dicts need to be kept track of externally
+    (although the record entry in the info dict is used to order the array).
 
-    args := (nldas_path, noahlsm_path, nldas_records, noahlsm_records)
+    args := (nldas_path, noahlsm_path,
+             nldas_info, noahlsm_info,
+             nldas_records, noahlsm_records)
     """
     all_data = []
-    nldas_path, noahlsm_path, nldas_records, noahlsm_records = args
+    nldas_path, noahlsm_path, nldas_info, noahlsm_info, \
+            nldas_records, noahlsm_records = args
     try:
         # append requested records in order
-        tmp_data,tmp_info,_ = grib_tools.get_grib1_data(nldas_path)
+        tmp_data = grib_tools.get_grib1_grid(nldas_path)
         for r in nldas_records:
             all_data.append(next(
                 tmp_data[i].data for i in range(len(tmp_data))
-                if tmp_info[i]["record"]==r
+                if nldas_info[i]["record"]==r
                 ))
-        tmp_data,tmp_info,_ = grib_tools.get_grib1_data(noahlsm_path)
+    except Exception as e:
+        print(f"FAILED: {nldas_path.name}")
+        raise e
+    try:
+        tmp_data = grib_tools.get_grib1_grid(noahlsm_path)
         for r in noahlsm_records:
             all_data.append(next(
                 tmp_data[i].data for i in range(len(tmp_data))
-                if tmp_info[i]["record"]==r
+                if noahlsm_info[i]["record"]==r
                 ))
+        #print(f"Extracted from {noahlsm_path.name}")
     except Exception as e:
-        #print(f"FAILED: {args[0]}")
-        print(e)
-    return np.dstack(all_data)
+        print(f"FAILED: {noahlsm_path.name}")
+        raise e
+    ## Return the combined features as a (1, y, x, f) array
+    return np.expand_dims(np.dstack(all_data), axis=0)
 
 if __name__=="__main__":
     ## Directories should contain only files that should be loaded to the hdf5
-    data_dir = Path("data")
-    nldas_grib_dir = data_dir.joinpath("nldas2_2021")
-    noahlsm_grib_dir = data_dir.joinpath("noahlsm_2021")
-    out_file = data_dir.joinpath("feats_2021.hdf5")
+    #data_dir = Path("data")
+    year = 2020
+    data_dir = Path("/rstor/mdodson/thesis/")
+    out_dir = Path("data")
+    nldas_grib_dir = data_dir.joinpath(f"nldas2/{year}")
+    noahlsm_grib_dir = data_dir.joinpath(f"noahlsm/{year}")
+    out_file = out_dir.joinpath(f"feats_{year}.hdf5")
 
     ## from old domain
     #v_bounds = slice(64,192)
@@ -188,5 +148,6 @@ if __name__=="__main__":
             nldas_grib_dir=nldas_grib_dir,
             noahlsm_grib_dir=noahlsm_grib_dir,
             out_h5file=out_file,
-            workers=4,
+            workers=7,
+            wgrib_bin="/rhome/mdodson/.micromamba/bin/wgrib",
             )
