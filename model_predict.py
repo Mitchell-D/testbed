cdea3fce54a88cd2ed7133c4283095c97306da46
Mitchell_D <mtd0012@uah.edu>
2024-01-23 20:04:06 -0600

    Added a prediction hdf5 dataset for the predicted features in the window time

--- model_predict.py
+++ model_predict.py
@@ -58,6 +58,12 @@ def model_predict(model_path, sample_h5, pred_h5, chunk_size=1000):
             chunks=(chunk_size,cfg["horizon_size"],len(cfg["pred_feats"])),
             compression="gzip"
             )
+    d_wdw = g_pred.create_dataset(
+            name="window",
+            shape=(num_smpl,cfg["window_size"],len(cfg["pred_feats"])),
+            chunks=(chunk_size,cfg["window_size"],len(cfg["pred_feats"])),
+            compression="gzip"
+            )
     ## sample index
     d_sidx = g_pred.create_dataset(
             name="sample_idx",
@@ -72,26 +78,36 @@ def model_predict(model_path, sample_h5, pred_h5, chunk_size=1000):
             )
 
     pred_idx = 0
+    wdw_pred_idxs = tuple(
+            cfg["window_feats"].index(f) for f in cfg["pred_feats"])
+    print(f"Window indeces: {wdw_pred_idxs}")
+    ## Note that chunks is a list of integers representing chunk sizes
     while len(chunks):
         print(chunks)
         W,H,S,P,T,sample_idxs,pivot_idxs = [],[],[],[],[],[],[]
+        W_pred = [] ## store a list of prediction feats in the window
         cur_chunk = chunks.pop(0)
         ## Extract and consolidate all the sample components
         for i in range(cur_chunk):
             X,Y,idxs = next(sample_gen)
             W.append(X["window"])
+            W_pred.append(W[-1][...,wdw_pred_idxs])
             H.append(X["horizon"])
             S.append(X["static"])
             sample_idxs.append(idxs[1])
             pivot_idxs.append(idxs[2])
             T.append(Y)
         W = np.stack(W, axis=0)
+        W_pred = np.stack(W_pred, axis=0)
         H = np.stack(H, axis=0)
         S = np.stack(S, axis=0)
         T = np.stack(T, axis=0)
         P = model({"window":W, "horizon":H, "static":S})
+        ## Save predicted features in the window and horizon range
         d_pred[pred_idx:pred_idx+cur_chunk,...] = P * out_mean + out_stdev
         d_truth[pred_idx:pred_idx+cur_chunk,...] = T * out_mean + out_stdev
+        d_wdw[pred_idx:pred_idx+cur_chunk,...] = W_pred * out_mean + out_stdev
+        #d_stat[pred_idx:pred_idx+cur_chunk,...] = S * out_mean + out_stdev
         d_pidx[pred_idx:pred_idx+cur_chunk] = np.array(pivot_idxs)
         d_sidx[pred_idx:pred_idx+cur_chunk] = np.array(sample_idxs)
         f_pred.flush()
@@ -130,6 +146,7 @@ def model_roll_chunks(model_path, sample_h5, pred_h5,
     ## Initialize a new hdf5 file for storing predictions
     f_pred = h5py.File(pred_h5.as_posix(), "w-", rdcc_nbytes=200*1024**2)
     g_pred = f_pred.create_group("/data")
+    g_pred.attrs["config"] = json.dumps(cfg)
     d_pred = g_pred.create_dataset(
             name="prediction",
             shape=(num_smpl,horizon_size,len(cfg["pred_feats"])),
@@ -140,6 +157,12 @@ def model_roll_chunks(model_path, sample_h5, pred_h5,
             name="truth",
             shape=(num_smpl,horizon_size,len(cfg["pred_feats"])),
             chunks=(chunk_size,horizon_size,len(cfg["pred_feats"])),
+            compression="gzip"
+            )
+    d_wdw = g_pred.create_dataset(
+            name="window",
+            shape=(num_smpl,cfg["window_size"],len(cfg["pred_feats"])),
+            chunks=(chunk_size,cfg["window_size"],len(cfg["pred_feats"])),
             compression="gzip"
             )
     ## sample index
@@ -155,15 +178,21 @@ def model_roll_chunks(model_path, sample_h5, pred_h5,
             chunks=chunk_size,
             )
 
+    wdw_pred_idxs = tuple(
+            cfg["window_feats"].index(f) for f in cfg["pred_feats"])
     pred_idx = 0
+    ## For each sample chunk, make predictions and consolidate all predicted
+    ## features in the window, horizon truth, and horizon predictions
     while len(chunks):
         print(chunks)
         W,H,S,P,T,sample_idxs,pivot_idxs = [],[],[],[],[],[],[]
+        W_pred = []
         cur_chunk = chunks.pop(0)
         ## Extract and consolidate all the sample components
         for i in range(cur_chunk):
             X,Y,idxs = next(sample_gen)
             W.append(X["window"])
+            W_pred.append(X["window"][...,wdw_pred_idxs])
             H.append(X["horizon"])
             S.append(X["static"])
             T.append(Y)
@@ -171,6 +200,7 @@ def model_roll_chunks(model_path, sample_h5, pred_h5,
             sample_idxs.append(idxs[1])
             pivot_idxs.append(idxs[2])
         W = np.stack(W, axis=0)
+        W_pred = np.stack(W_pred, axis=0)
         H = np.stack(H, axis=0)
         S = np.stack(S, axis=0)
         T = np.stack(T, axis=0)
@@ -192,6 +222,7 @@ def model_roll_chunks(model_path, sample_h5, pred_h5,
         ## Update all the file fields and save the file
         d_pred[pred_idx:pred_idx+cur_chunk,...] = P * out_mean + out_stdev
         d_truth[pred_idx:pred_idx+cur_chunk,...] = T * out_mean + out_stdev
+        d_wdw[pred_idx:pred_idx+cur_chunk,...] = W_pred * out_mean + out_stdev
         d_pidx[pred_idx:pred_idx+cur_chunk] = np.array(pivot_idxs)
         d_sidx[pred_idx:pred_idx+cur_chunk] = np.array(sample_idxs)
         f_pred.flush()
@@ -241,15 +272,16 @@ def model_roll(model_path, sample_h5s, horizon_size=12, num_samples=1000):
 
 if __name__=="__main__":
     #sample_h5 = Path("data/shuffle_2018.h5")
-    sample_h5 = Path("/rstor/mdodson/thesis/shuffle_SEUS_2018.h5")
+    sample_h5 = Path("/rstor/mdodson/thesis/shuffle_2018.h5")
+    #sample_h5 = Path("/rstor/mdodson/thesis/shuffle_SEUS_2018.h5")
 
     """ --=( Self-cycling models )=-- """
     #model_dir = Path(f"data/models/dense-1")
     #model_path = model_dir.joinpath(f"dense-1_38_0.07.hdf5")
     #model_dir = Path(f"data/models/lstm-s2s-2")
     #model_path = model_dir.joinpath(f"lstm-s2s-2_015_0.24.hdf5")
-    #model_dir = Path(f"data/models/lstm-rec-1")
-    #model_path = model_dir.joinpath(f"lstm-rec-1_061_0.08.hdf5")
+    model_dir = Path(f"data/models/lstm-rec-1")
+    model_path = model_dir.joinpath(f"lstm-rec-1_061_0.08.hdf5")
 
     #model_dir = Path(f"data/models-seus/lstm-rec-seus-0")
     #model_path = model_dir.joinpath(f"lstm-rec-seus-0_115_0.05.hdf5")
@@ -258,16 +290,17 @@ if __name__=="__main__":
     #model_dir = Path(f"data/models-seus/lstm-s2s-seus-0")
     #model_path = model_dir.joinpath(f"lstm-s2s-seus-0_028_0.18.hdf5")
 
-    '''
+    #'''
     cfg = mm.load_config(model_dir)
     P = model_roll_chunks(
             model_path=model_path,
             sample_h5=sample_h5,
             pred_h5=Path(
-                f"/rstor/mdodson/thesis/pred_2018_SEUS_{cfg['model_name']}.h5"),
+                #f"/rstor/mdodson/thesis/pred_2018_SEUS_{cfg['model_name']}.h5"),
+                f"/rstor/mdodson/thesis/pred_2018_{cfg['model_name']}_V2.h5"),
             chunk_size=512**2
             )
-    '''
+    #'''
 
     """ --=( Multi-horizon models )=-- """
     #model_dir = Path(f"data/models/lstm-s2s-5")
@@ -277,16 +310,17 @@ if __name__=="__main__":
 
     #model_dir = Path(f"data/models-seus/tcn-seus-0")
     #model_path = model_dir.joinpath(f"tcn-seus-0_099_0.04.hdf5")
-    model_dir = Path(f"data/models-seus/lstm-s2s-seus-1")
-    model_path = model_dir.joinpath(f"lstm-s2s-seus-1_004_0.47.hdf5")
+    #model_dir = Path(f"data/models-seus/lstm-s2s-seus-1")
+    #model_path = model_dir.joinpath(f"lstm-s2s-seus-1_004_0.47.hdf5")
 
-    #'''
+    '''
     cfg = mm.load_config(model_dir)
     P = model_predict(
             model_path=model_path,
             sample_h5=sample_h5,
             pred_h5=Path(
-                f"/rstor/mdodson/thesis/pred_2018_SEUS_{cfg['model_name']}.h5"),
+                #f"/rstor/mdodson/thesis/pred_2018_SEUS_{cfg['model_name']}.h5"),
+                f"/rstor/mdodson/thesis/pred_2018_{cfg['model_name']}_V2.h5"),
             chunk_size=256**2
             )
-    #'''
+    '''
