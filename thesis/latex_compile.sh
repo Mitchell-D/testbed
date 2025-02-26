745ce3829d4e0d6c5abcc99451f648061a5e607b
Mitchell-D <mtd0012@uah.edu>
2025-02-25 20:20:13 -0600

    updates to methodology, and fixed git disaster (hopefully)

---
+++ thesis/latex_compile.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+pdflatex -output-directory out dodson_masters-thesis.tex ; bibtex out/dodson_masters-thesis ; pdflatex -output-directory out dodson_masters-thesis.tex ; pdflatex -output-directory out dodson_masters-thesis.tex
