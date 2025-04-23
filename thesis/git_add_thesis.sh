571c4239eefcc20158b08c1631398b85078c68c0
Mitchell-D <mtd0012@uah.edu>
2025-04-23 13:29:59 -0500

    some more writing progress

--- thesis/git_add_thesis.sh
+++ thesis/git_add_thesis.sh
@@ -5,5 +5,5 @@ git add frontmatter/*.tex
 git add frontmatter/Do\ Not\ Edit
 git add dodson_masters-thesis.tex
 git add dodson_masters-thesis.bib
-git add out/dodson_masters-thesis.pdf
+git add -f out/dodson_masters-thesis.pdf
 git add -f figures/*
--- thesis/latex_compile.sh
+++ thesis/latex_compile.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-pdflatex -output-directory out dodson_masters-thesis.tex ; bibtex out/dodson_masters-thesis ; pdflatex -output-directory out dodson_masters-thesis.tex ; pdflatex -output-directory out dodson_masters-thesis.tex
+pdflatex -output-directory out dodson_masters-thesis.tex ;
+bibtex out/dodson_masters-thesis ;
+pdflatex -output-directory out dodson_masters-thesis.tex ;
+pdflatex -output-directory out dodson_masters-thesis.tex
