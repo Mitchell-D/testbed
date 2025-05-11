7e2f210dd5e40c43924f1f6aa6054e728af8c66b
Mitchell-D <mtd0012@uah.edu>
2025-05-10 19:02:01 -0500

    diminutive hack change to slopetype plotting that made it work

--- scripts/plot_slopetype.py
+++ scripts/plot_slopetype.py
@@ -13,20 +13,27 @@ if __name__=="__main__":
     gdas_file = root_dir.joinpath("data/static").joinpath(
             "lis71_input_GDAStbot_viirsgvf_GDASforc.d01_conus3km.nc")
     ncd = nc4.Dataset(root_dir.joinpath(gdas_file))
+    int_data = ncd["SLOPETYPE"][::-1]
+    print(np.unique(int_data))
+    print(int_data.shape)
+    int_data[0,0] = 8
+    print(np.unique(int_data))
     plotting.plot_geo_ints(
             #int_data=np.where(
             #    ncd["LANDMASK"][::-1], ncd["SLOPETYPE"][::-1], np.nan),
-            int_data=ncd["SLOPETYPE"][::-1],
+            int_data=int_data,
             #int_ticks=np.array(list(range(10)))+.5,
-            int_ticks=np.array([0,1,2,3,4,5,6,7,8,9])+.5,
-            int_labels=[0,1,2,3,4,5,6,7,8,9],
+            int_ticks=np.array(list(range(10)))*(9/10)+.5,
+            int_labels=list(map(str,range(10))),
             lat=ncd["lat"][::-1],
             lon=ncd["lon"][::-1],
             geo_bounds=None,
             fig_path=root_dir.joinpath(
                 f"figures/static/static_slopetype_3km.png"),
+            color_list=["white", "blue", "orange", "green", "red",
+                    "purple", "olive", "cyan", "pink", "gray"],
             plot_spec={
-                "cmap":"gist_ncar",
+                #"cmap":"gist_ncar",
                 "cbar_pad":0.02,
                 "cbar_orient":"horizontal",
                 "cbar_tick_rotation":90,
